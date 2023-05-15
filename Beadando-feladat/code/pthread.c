6d259189733c30d9d6464a5a58b0e509ef73f7b2
Kgyorgy <90448968+koradgy@users.noreply.github.com>
2023-05-15 13:44:29 +0200

    Pthread javítva

--- Beadando-feladat/code/pthread.c
+++ Beadando-feladat/code/pthread.c
@@ -1,91 +1,140 @@
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <limits.h>
 
+// Struktúra a szálparaméterek tárolásához
+typedef struct {
+    int start_index;
+    int end_index;
+    int* array;
+} ThreadArgs;
 
-unsigned int N;
-int threads;
-int* arr;
+// Segédfüggvény a két szám cserecseréjéhez
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-void* bubble_sort(void* thread)
-{
-    long rank = (long) thread;
-    int i, j, temp;
-    int start = rank * (N / threads);
-    int end = (rank + 1) * (N / threads);
-    for (i = start; i < end; i++) 
-    {
-        for (j = start; j < end - i - 1; j++) 
-        {
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+unsigned int ARRAY_SIZE ;
+unsigned int NUM_THREADS ;
+
+// Szálkezelő függvény a bubblesort algoritmus végrehajtásához
+void* bubbleSort(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
+    int start = args->start_index;
+    int end = args->end_index;
+    int* array = args->array;
+
+    for (int i = start; i < end - 1; ++i) {
+        for (int j = 0; j < end - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                swap(&array[j], &array[j + 1]);
             }
         }
     }
-    return NULL;
+
+    pthread_exit(NULL);
 }
 
-
-int main()
+int main() 
 {
+    printf("size of the array: \n");
+    scanf("%u", &ARRAY_SIZE);
+    printf("number of threads:\n");
+    scanf("%u", &NUM_THREADS);
 
-
-    printf("Size of the array:\n");
-    scanf("%u",&N);
-
-    
-    printf("Number of threads\n");
-    scanf("%u",&threads);
-    
-    clock_t start = clock();
-    arr = malloc (N * sizeof(int));
+    int array[ARRAY_SIZE];
+    clock_t begin = clock();
     srand(time(NULL));
 
-    for(int i =0; i<N; i++)
-    {
-        arr[i] = rand() % 1000;
+    // Tömb elemek generálása véletlen számokkal
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        array[i] = rand() % 100;
     }
 
-  /*  for(int i = 0; i < N; i++)
-    {
-        printf("%d, ",arr[i]);
+    // Szálak inicializálása és indítása
+    pthread_t threads[NUM_THREADS];
+    ThreadArgs thread_args[NUM_THREADS];
+
+    int chunk_size = ARRAY_SIZE / NUM_THREADS;
+
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        thread_args[i].start_index = i * chunk_size;
+        thread_args[i].end_index = (i + 1) * chunk_size;
+        thread_args[i].array = array;
+
+        pthread_create(&threads[i], NULL, bubbleSort, (void*)&thread_args[i]);
+    }
+
+    // Szálak befejezésének várása
+    for (int i = 0; i < NUM_THREADS; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+
+    // Összefésülés
+    int step = 1;
+
+    while (step < ARRAY_SIZE) {
+        for (int i = 0; i < ARRAY_SIZE; i += 2 * step) {
+            int start = i;
+            int mid = i + step;
+            int end = i + 2 * step;
+
+            if (mid >= ARRAY_SIZE) {
+                continue;
+            }
+
+            if (end > ARRAY_SIZE) {
+                end = ARRAY_SIZE;
+            }
+
+            int merged_size = end - start;
+            int merged_array[merged_size];
+
+            int left = start;
+            int right = mid;
+            int k = 0;
+
+            while (left < mid && right < end) {
+                if (array[left] <= array[right]) {
+                    merged_array[k++] = array[left++];
+                } else {
+                    merged_array[k++] = array[right++];
+                }
+            }
+
+            while (left < mid) {
+                merged_array[k++] = array[left++];
+            }
+
+            while (right < end) {
+                merged_array[k++] = array[right++];
+            }
+
+            for (int j = 0; j < merged_size; ++j) {
+                array[start + j] = merged_array[j];
+            }
+        }
+
+        step *= 2;
+    }
+
+    // Rendezett tömb kiíratása
+   /* printf("Rendezett tömb: ");
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        printf("%d ", array[i]);
     }
     printf("\n");*/
 
-    //bubble sort
-
-    pthread_t* handles;
-    handles = malloc (threads * sizeof(pthread_t));
-
-    for (long thread = 0; thread < threads; thread++) 
-    {
-        pthread_create(&handles[thread], NULL, bubble_sort, (void*) thread);
-    }
-
-    for (long thread = 0; thread < threads; thread++) 
-    {
-        pthread_join(handles[thread], NULL);
-    }
-
-   /* for(int i = 0; i < N; i++)
-    {
-        printf("%d, ",arr[i]);
-    }   
-    printf("\n");*/
-
-    clock_t end = clock();
-    double runtime = (end - start)/(CLOCKS_PER_SEC/1000);
-    
-    FILE *fp = fopen("times.txt","a");
-    fprintf(fp, "Pthread %d threads: %.2f ms\n",threads, runtime);
-    fflush(fp);
-    fclose(fp);
-
-    free(handles);
-    free(arr);
+        clock_t stop = clock();
+        double runtime = (stop - begin)/(CLOCKS_PER_SEC/1000);
+        FILE *fp = fopen("times.txt","a");
+        fprintf(fp, "pthread  threads: %.2f ms\n", runtime);
+        fflush(fp);
+        fclose(fp);
 
     return 0;
 }
